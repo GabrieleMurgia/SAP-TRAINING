951a928e3c1e2c4ff0e7317cd91d135707a18676
Murgia Gabriele <gabriele.murgia@innovatesapp.com>
2023-09-09 12:46:28 +0200

    first control have been added to the view

--- UI5 Training/SAP UI5 Course/firstproject/webapp/controller/View1.controller.js
+++ UI5 Training/SAP UI5 Course/firstproject/webapp/controller/View1.controller.js
@@ -1,15 +1,26 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/Text"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.m.Text} Text
      */
-    function (Controller) {
+    function (Controller, Text) {
         "use strict";
 
         return Controller.extend("firstproject.controller.View1", {
             onInit: function () {
+                // Crea un nuovo oggetto Text
+                var oText = new Text({
+                    text: "Ciao"
+                });
 
+                // Ottieni la pagina dalla view
+                var oPage = this.getView().byId("page");
+
+                // Aggiungi il testo alla pagina
+                oPage.addContent(oText);
             }
         });
     });
